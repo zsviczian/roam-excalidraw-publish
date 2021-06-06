a9740e64af0274a646e7313f0d1f192332e34857
Zsolt Viczian <viczian.zsolt@gmail.com>
2021-06-06 16:25:13 +0200

    replaced php in all cases

--- beta/loader.js
+++ beta/loader.js
@@ -43,7 +43,7 @@ if (typeof window.ExcalidrawWrapper === 'undefined') {
     ExcalidrawConfig.log('loader.js','rootPath:',ExcalidrawConfig.rootPath,'channel:',ExcalidrawConfig.channel,'debug?',ExcalidrawConfig.DEBUG);
       if (getClojureNS(ExcalidrawConfig.sketchingUID) != ExcalidrawConfig.cljCodeVersion) {
         ExcalidrawConfig.log('loader.js','Need to update CLJS script. Starting roam-excalidraw-cljs-loader');
-        ExcalidrawConfig.addScriptToPage( 'roam-excalidraw-cljs-loader',  ExcalidrawConfig.rootPath + 'get_dev.php?c='+ExcalidrawConfig.channel);
+        ExcalidrawConfig.addScriptToPage( 'roam-excalidraw-cljs-loader',  ExcalidrawConfig.rootPath + ExcalidrawConfig.channel + '/clj-deploy-package.js?v='+ExcalidrawConfig.cljCodeVersion);
       }
       else {
         ExcalidrawConfig.log('loader.js','cljs NS is up to date');
